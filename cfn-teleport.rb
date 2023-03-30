6be84002d3ed294e0d3f1ab396ecbc7be8a93fad
Daniel Schroeder <deemes79@googlemail.com>
2023-03-30 16:40:37 +0000

    Release 0.3.0

--- cfn-teleport.rb
+++ cfn-teleport.rb
@@ -4,16 +4,16 @@
 class CfnTeleport < Formula
   desc "A command line-tool which can move CloudFormation resources between stacks"
   homepage "https://github.com/udondan/cfn-teleport"
-  version "9.9.9"
+  version "0.3.0"
   license "Apache-2.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/udondan/cfn-teleport/releases/download/v9.9.9/cfn-teleport-x86_64-apple-darwin-v9.9.9.dmg"
-    sha256 "afaacd98ff24ba561ad3457f6803584b787b58d039abad932eff129ddc9a90e2"
+    url "https://github.com/udondan/cfn-teleport/releases/download/v0.3.0/cfn-teleport-x86_64-apple-darwin-v0.3.0.dmg"
+    sha256 "38a1c6ed3f9cbeeb740f740b5a873b9f79890a57cf5809e1689dd94a000ab4d0"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/udondan/cfn-teleport/releases/download/v9.9.9/cfn-teleport-aarch64-apple-darwin-v9.9.9.dmg"
-    sha256 "750ac04c97a8e4ced6842f6b885f77ee671c50b8c79da6a1af241e0d671f475a"
+    url "https://github.com/udondan/cfn-teleport/releases/download/v0.3.0/cfn-teleport-aarch64-apple-darwin-v0.3.0.dmg"
+    sha256 "60268354f83c87f0b33c7a1106370bcc5aedc67c80573b7679a04abd85241832"
   end
 
   def install
