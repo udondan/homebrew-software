90d580b457f423e25f473320697434a2a3ca30af
Daniel Schroeder <deemes79@googlemail.com>
2023-04-29 13:15:14 +0000

    Release 9.9.9

--- cfn-teleport.rb
+++ cfn-teleport.rb
@@ -9,11 +9,11 @@ class CfnTeleport < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/udondan/cfn-teleport/releases/download/v9.9.9/cfn-teleport-x86_64-apple-darwin-v9.9.9.dmg"
-    sha256 "6bbfce343c0b9f4cf17f84d3edde4f4c4a1531a1f447ff653b1892b141214020"
+    sha256 "f533a0c09fde7611304ce5821bac4817345e071b73eab858743061cb1824c92f"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/udondan/cfn-teleport/releases/download/v9.9.9/cfn-teleport-aarch64-apple-darwin-v9.9.9.dmg"
-    sha256 "e1525e7ba70389062b3341d30257741c870cdbc9b42261d9053038b6ed0b9543"
+    sha256 "9080546748cdd21764d0522d6edb6afcc06a4c5d60de6a7a854f92f2c934a6d5"
   end
 
   def install
