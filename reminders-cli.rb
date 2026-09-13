18b450b5349004bbe37f56d3973e0e3c8b7a96b8
udondan <deemes79@googlemail.com>
2026-09-13 21:49:50 +0000

    Release 3.2.0

--- reminders-cli.rb
+++ reminders-cli.rb
@@ -1,10 +1,10 @@
 class RemindersCli < Formula
   desc "Interact with macOS Reminders from the command line"
   homepage "https://github.com/udondan/reminders-cli"
-  version "3.1.1"
+  version "3.2.0"
   license "MIT"
-  url "https://github.com/udondan/reminders-cli/releases/download/v3.1.1/reminders.tar.gz"
-  sha256 "d819d382476252c14da05154e918b403c093caafea321d4425e295a45f9b4a81"
+  url "https://github.com/udondan/reminders-cli/releases/download/v3.2.0/reminders.tar.gz"
+  sha256 "a4e7e1e6780e7819e262e1d13bffde245fbd92db40df5ea0f6cd3cb9a86f20a9"
 
   def install
     bin.install "reminders"
